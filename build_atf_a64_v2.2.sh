03db68a88c66715c5d35664d62b52bf04d7fe496
LT21-LINUX <lt21.linux@gmail.com>
2022-05-04 13:34:24 +0800

    arm-trusted-firmware-sunxi-mainline

---
+++ build_atf_a64_v2.2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TOPDIR=`pwd`
+TOOLCHAIN=gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
+export PATH=$TOPDIR/toolchains/$TOOLCHAIN/bin:$PATH
+cd arm-trusted-firmware-sunxi-mainline/v2.2
+#
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+make PLAT=sun50i_a64 DEBUG=1 bl31
---
+++ build_atf_h6.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TOPDIR=`pwd`
+TOOLCHAIN=gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
+export PATH=$TOPDIR/toolchains/$TOOLCHAIN/bin:$PATH
+cd arm-trusted-firmware-sunxi-mainline/master
+#
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+make PLAT=sun50i_h6 DEBUG=1 bl31
