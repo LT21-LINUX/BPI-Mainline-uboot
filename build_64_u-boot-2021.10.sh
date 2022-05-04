ed67cd136c2b9510bf17816c3331326ef9428b74
LT21-LINUX <lt21.linux@gmail.com>
2022-05-04 16:49:53 +0800

    update: build_64_u-boot-2021.10.sh

--- build_64_u-boot-2021.10.sh
+++ build_64_u-boot-2021.10.sh
@@ -4,6 +4,10 @@ TOOLCHAIN=gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu
 export PATH=$TOPDIR/toolchains/$TOOLCHAIN/bin:$PATH
 #cd u-boot-sunxi
 cd u-boot-2021.10
-#./bpi-m2p-h5.sh
-./bpi-m64.sh
-#./opi3.sh
+(./bpi-m2p-h5.sh)
+(./bpi-m64.sh)
+(./opi3-lts.sh)
+(./opi3.sh)
+(./opi-lite2.sh)
+(./opi-z2p.sh)
+(./opi-zero2.sh)
