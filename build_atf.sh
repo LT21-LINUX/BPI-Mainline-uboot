619846b04a11f5ba8fbf96195d14698a6c8cf39b
LT21-LINUX <lt21.linux@gmail.com>
2022-05-04 15:47:32 +0800

    update: build_atf.sh

--- build_atf.sh
+++ build_atf.sh
@@ -4,3 +4,10 @@ TOOLCHAIN=gcc-linaro-6.4.1-2017.11-x86_64_aarch64-linux-gnu
 export PATH=$TOPDIR/$TOOLCHAIN/bin:$PATH
 cd arm-trusted-firmware/sunxi
 ./bpi-sunxi64.sh
+
+cd $TOPDIR
+(./build_atf_a64.sh)
+(./build_atf_a64_v2.2.sh)
+(./build_atf_h616.sh)
+(./build_atf_h6.sh)
+(./build_atf_h6_v2.2.sh)
